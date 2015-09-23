5de305acc9456f4f9b19316c4445d88d62ac26e2
Jonas Aschenbrenner <jonas.aschenbrenner@gmail.com>
2015-09-23 13:45:03 +0200

    Adds missing test dependency: underscore

--- package.js
+++ package.js
@@ -29,6 +29,7 @@ Package.onUse(function (api) {
 })
 
 Package.onTest(function(api) {
+  api.use('underscore')
   api.use('grigio:babel@0.1.4')
   api.use('sanjo:jasmine@0.16.4')
   api.use('velocity:helpers')
